dfe20a499f95e2ef751672e2a3728fb77d5502fb
Faiz <fabbasi@andrew.cmu.edu>
2012-04-17 14:43:53 -0400

    Gallery view is now hooked up to the database

--- src/com/testapp/app/Gallery.java
+++ src/com/testapp/app/Gallery.java
@@ -10,6 +10,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +32,7 @@ public class Gallery extends Activity {
    
    List<Hashtable> dataHashTable = new ArrayList<Hashtable>();
    List<Hashtable> hashtable = null;
+   List<Drawable> images = new ArrayList<Drawable>();
    
 	   
    @Override
@@ -48,16 +51,67 @@ public class Gallery extends Activity {
         hashtable = dm.selectHashtable();
         
         for(Hashtable table : hashtable){
-        	byte[] bytes = (byte[]) table.get("Picture");
-        	list.add(bytes);
+        	byte[] bytes = (byte[]) table.get("picture");
+        	int h = 48; // height in pixels
+        	int w = 48; // width in pixels    
+        	Bitmap largeBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        	Bitmap scaled = Bitmap.createScaledBitmap(largeBitmap, h, w, true);
+        	Drawable drw = new BitmapDrawable(scaled);
+        	images.add(drw);
         }
    }
+   
+   public void onResume() {
+	   super.onResume();
+//       hashtable = dm.selectHashtable();
+//       for(Hashtable table : hashtable){
+//       	byte[] bytes = (byte[]) table.get("picture");
+//       	ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+//       	Drawable drw = Drawable.createFromStream(is, "articleImage");
+//       	for(int i = 0; i < images.size(); i++)
+//       	{
+//       		if(images.get(i).equals(drw))
+//       		{
+//       				
+//       		}
+//       		else{
+//       			images.add(drw);
+//       		}
+//       	}
+//       }
+   }
+   
+   @Override
+   protected void onPause() {
+   super.onPause();
+
+   unbindDrawables(findViewById(R.id.myGrid));
+   System.gc();
+   }
+
+   private void unbindDrawables(View view) {
+       if (view.getBackground() != null) {
+       view.getBackground().setCallback(null);
+       }
+       if (view instanceof ViewGroup) {
+           for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+           unbindDrawables(((ViewGroup) view).getChildAt(i));
+           }
+       try {
+           ((ViewGroup) view).removeAllViews();
+    	   }
+    	   catch (UnsupportedOperationException mayHappen) {
+    	   // AdapterViews, ListViews and potentially other ViewGroups donÕt support the removeAllViews operation
+    	   }
+       }
+   }
+   
    public class ImageAdapter extends BaseAdapter {
         public ImageAdapter(Context c) {
                 mContext = c;
         }
         public int getCount() {
-                return mThumbIds.length;
+                return images.size();
         }
         public Object getItem(int position) {
                 return position;
@@ -76,20 +130,12 @@ public class Gallery extends Activity {
                 } else {
                    imageView = (ImageView) convertView;
                 }
-                imageView.setImageResource(mThumbIds[position]);
-//                InputStream is = new ByteArrayInputStream(list.get(position));
-//                Bitmap bmp = BitmapFactory.decodeStream(is);
-//                imageView.setImageBitmap(bmp);
+
+                imageView.setImageDrawable(images.get(position));
+                
                 return imageView;
+        	
         }
         private Context mContext;
-        private Integer[] mThumbIds = {
-                R.drawable.buttondown, R.drawable.dress,
-                R.drawable.pants, R.drawable.shorts,
-                R.drawable.tshirt, R.drawable.sweatpants,
-                R.drawable.sweatshirt, R.drawable.jacket,
-                R.drawable.jeans, R.drawable.khakis,
-                R.drawable.sundress,
-        };
     }
 }
