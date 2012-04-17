797094fea5a06b0c5702bada71d47ceee34277d5
Nina Xu <ninax@andrew.cmu.edu>
2012-04-17 17:48:41 -0400

    Camera UI + functionality and icons

--- gen/com/testapp/app/R.java
+++ gen/com/testapp/app/R.java
@@ -11,40 +11,49 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int buttondown=0x7f020000;
-        public static final int dress=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int jacket=0x7f020003;
-        public static final int jeans=0x7f020004;
-        public static final int khakis=0x7f020005;
-        public static final int pants=0x7f020006;
-        public static final int shirt=0x7f020007;
-        public static final int shorts=0x7f020008;
-        public static final int splash_background=0x7f020009;
-        public static final int sundress=0x7f02000a;
-        public static final int sweatpants=0x7f02000b;
-        public static final int sweatshirt=0x7f02000c;
-        public static final int tshirt=0x7f02000d;
+        public static final int add_how=0x7f020000;
+        public static final int buttondown=0x7f020001;
+        public static final int camera_icon=0x7f020002;
+        public static final int dress=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_tabs_add=0x7f020005;
+        public static final int ic_tabs_closet=0x7f020006;
+        public static final int ic_tabs_match=0x7f020007;
+        public static final int jacket=0x7f020008;
+        public static final int jeans=0x7f020009;
+        public static final int khakis=0x7f02000a;
+        public static final int launcher_icon=0x7f02000b;
+        public static final int pants=0x7f02000c;
+        public static final int shirt=0x7f02000d;
+        public static final int shorts=0x7f02000e;
+        public static final int splash_background=0x7f02000f;
+        public static final int sundress=0x7f020010;
+        public static final int sweatpants=0x7f020011;
+        public static final int sweatshirt=0x7f020012;
+        public static final int tshirt=0x7f020013;
     }
     public static final class id {
-        public static final int Button01add=0x7f070012;
-        public static final int Button01home=0x7f070013;
-        public static final int LinearLayout01=0x7f07000e;
-        public static final int LinearLayout02=0x7f070011;
+        public static final int Button01add=0x7f070015;
+        public static final int Button01home=0x7f070016;
+        public static final int LinearLayout01=0x7f070011;
+        public static final int LinearLayout02=0x7f070014;
+        public static final int bSetWall=0x7f07000f;
         public static final int button1=0x7f07000a;
-        public static final int button1_label=0x7f07000f;
+        public static final int button1_label=0x7f070012;
         public static final int button2=0x7f07000b;
         public static final int button3=0x7f07000c;
-        public static final int delete=0x7f070015;
-        public static final int edit=0x7f070014;
+        public static final int delete=0x7f070018;
+        public static final int edit=0x7f070017;
         public static final int editText1=0x7f070008;
+        public static final int ibTakePic=0x7f07000e;
         public static final int imageView1=0x7f070000;
+        public static final int ivReturnedPic=0x7f07000d;
         public static final int linearLayout1=0x7f070003;
         public static final int myGrid=0x7f070009;
-        public static final int name=0x7f070010;
+        public static final int name=0x7f070013;
         public static final int selection=0x7f070002;
         public static final int selection2=0x7f070001;
-        public static final int text=0x7f07000d;
+        public static final int text=0x7f070010;
         public static final int textView1=0x7f070004;
         public static final int textView2=0x7f070005;
         public static final int textView3=0x7f070006;
@@ -57,9 +66,10 @@ public final class R {
         public static final int gallery=0x7f030003;
         public static final int list=0x7f030004;
         public static final int main=0x7f030005;
-        public static final int sampler=0x7f030006;
-        public static final int save=0x7f030007;
-        public static final int splash=0x7f030008;
+        public static final int photo=0x7f030006;
+        public static final int sampler=0x7f030007;
+        public static final int save=0x7f030008;
+        public static final int splash=0x7f030009;
     }
     public static final class menu {
         public static final int view=0x7f060000;
