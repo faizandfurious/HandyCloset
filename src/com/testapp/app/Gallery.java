b46feda4e2d1bebb3cdee928c47a7d607611332e
Nack Choon Jung <nackchoon@cmu.edu>
2012-04-17 21:29:41 -0400

    Back

--- src/com/testapp/app/Gallery.java
+++ src/com/testapp/app/Gallery.java
@@ -1,7 +1,6 @@
 package com.testapp.app;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -9,8 +8,6 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -22,13 +19,10 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
+public class Gallery extends Activity {
 
-public class Gallery extends Activity implements View.OnClickListener{
-	
-	ImageView imageView;
-	
-	
-	   
+
+
    DataManipulator dm;
    
    List<byte[]> list = new ArrayList<byte[]>();
@@ -37,21 +31,16 @@ public class Gallery extends Activity implements View.OnClickListener{
    List<Hashtable> dataHashTable = new ArrayList<Hashtable>();
    List<Hashtable> hashtable = null;
    
-	   
+
    @Override
    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery);
-        
         GridView g = (GridView) findViewById(R.id.myGrid);
         g.setAdapter(new ImageAdapter(this));
-        
-        imageView = (ImageView)findViewById(R.id.imageView1);
         g.setOnItemClickListener(new OnItemClickListener() {
-        	@Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-              Toast.makeText(getBaseContext(), "" + position, Toast.LENGTH_SHORT).show();
-              	imageView.setImageResource(mThumbIds[position]);
+           public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+              Toast.makeText(Gallery.this, "" + position, Toast.LENGTH_SHORT).show();
            }
         });
 
@@ -64,33 +53,18 @@ public class Gallery extends Activity implements View.OnClickListener{
         }
    }
    public class ImageAdapter extends BaseAdapter {
-	   
-	   private Context ctx;
-	   int imageBackground;
-	   
         public ImageAdapter(Context c) {
-        	ctx = c;
-        	TypedArray ta = obtainStyledAttributes(R.styleable.Gallery);
-            imageBackground = ta.getResourceId(R.styleable.Gallery_android_galleryItemBackground, 1);
-            ta.recycle();
+                mContext = c;
         }
-        
-        @Override
         public int getCount() {
                 return mThumbIds.length;
         }
-        
-        @Override
         public Object getItem(int position) {
                 return position;
         }
-        
-        @Override
         public long getItemId(int position) {
                 return position;
         }
-        
-        @Override
         public View getView(int position, View convertView, ViewGroup parent) {
                 ImageView imageView;
                 if (convertView == null) {
@@ -109,6 +83,13 @@ public class Gallery extends Activity implements View.OnClickListener{
                 return imageView;
         }
         private Context mContext;
+        private Integer[] mThumbIds = {
+                R.drawable.buttondown, R.drawable.dress,
+                R.drawable.pants, R.drawable.shorts,
+                R.drawable.tshirt, R.drawable.sweatpants,
+                R.drawable.sweatshirt, R.drawable.jacket,
+                R.drawable.jeans, R.drawable.khakis,
+                R.drawable.sundress,
+        };
     }
-
 }
