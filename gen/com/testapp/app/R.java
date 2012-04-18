39ff6b0be178047022e45469063712916eb7c07f
Nack Choon Jung <nackchoon@cmu.edu>
2012-04-17 21:27:57 -0400

    fixed?

--- gen/com/testapp/app/R.java
+++ gen/com/testapp/app/R.java
@@ -82,8 +82,6 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050001;
-        public static final int delete=0x7f050005;
-        public static final int edit=0x7f050004;
         public static final int hello=0x7f050000;
         public static final int name=0x7f050003;
         public static final int shirt=0x7f050002;
