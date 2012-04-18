6a436a3a92509ab41b8dc93ede09b4e6b5fc1039
Nack Choon Jung <nackchoon@cmu.edu>
2012-04-17 20:15:45 -0400

    menu layouts added for view

---
+++ src/com/testapp/app/ViewClothing.java
@@ -0,0 +1,37 @@
+package com.testapp.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ViewClothing extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.view, menu);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.edit: 
+			Toast.makeText(this, "Edit menu", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.delete:
+			Toast.makeText(this, "Delete menu", Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}
